4afebbd3c32ed37021cf3935ae7b9ef613a69eb1
ss_groove <vsevolodvolk2010@gmail.com>
2026-06-16 12:22:15 +0300

    paths fixes

--- FightClubBot/bot.py
+++ FightClubBot/bot.py
@@ -1009,7 +1009,7 @@ async def on_ready():
 # Запуск бота
 if __name__ == "__main__":
     TOKEN = os.getenv('BOT_TOKEN_RULER')
-    manager = DB_Manager('fg_db.db')
+    manager = DB_Manager('"/data/fg_db.db"')
     if TOKEN:
         bot.run(TOKEN)
     else:
